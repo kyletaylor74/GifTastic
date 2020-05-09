9d8a2a42ad4595331a8ab34c4c7995f516ee7277
thatyoungcomputer <kyledltaylor@gmail.com>
2020-05-09 16:46:45 -0400

    attcahed the gifs to the correct button

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ $(document).ready(function(){
 
     let videogames = ["Assassins Creed", "Super Mario Bros.", "Call of Duty"];
 
-    function populateButtons(arrayToUse, classToAdd, areaToAddTo){
+    function vgButtons(arrayToUse, classToAdd, areaToAddTo){
         $(areaToAddTo).empty();
 
-        for (let i = 0; i < arrayToUse; i++){
+        for (let i = 0; i < arrayToUse.length; i++){
             let a = $("<button>");
             a.addClass(classToAdd);
             a.attr("data-type", arrayToUse[i]);
             a.text(arrayToUse[i]);
+
             $(areaToAddTo).append(a);
         }
     }
 
-    $(document).on("click",".vg-buttons", function(){
+    $(document).on("click",".vg-button", function(){
         $("#images").empty();
 
-        $(".food-button").removeClass("active");
+        $(".vg-button").removeClass("active");
         $(this).addClass("active");
 
         let type = $(this).attr("data-type");
-        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=mzkbJko7cGJQ1CNXD8RDLIa4qUqAS7F5=10";
+        let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=mzkbJko7cGJQ1CNXD8RDLIa4qUqAS7F5&limit=10";
 
 
         $.ajax({
@@ -41,7 +42,7 @@ $(document).ready(function(){
 
                 let animated = results[i].images.fixed_height.url;
 
-                let still = results[i].images.fixed_height.url
+                let still = results[i].images.fixed_height_still.url;
 
                 let vgImages = $("<img>");
                 vgImages.attr("src", still);
@@ -51,8 +52,8 @@ $(document).ready(function(){
                 vgImages.addClass("vg-image");
 
 
-                foodDiv.append(a);
-                foodDiv.append(vgImages);
+                vgDiv.append(p);
+                vgDiv.append(vgImages);
 
                 $("#images").append(vgDiv);
 
@@ -60,11 +61,37 @@ $(document).ready(function(){
 
 
             }
-        })
-    })
+        });
+    });
 
 
-        $(document).on("click")
+        $(document).on("click", ".vg-image", function(){
+            let state = $(this).attr("data-state");
+
+            if(state === "still"){
+                $(this).attr("src",$(this).attr("data-animate"));
+                $(this).attr("data-state","animate");
+            }
+            else{
+                $(this).attr("src",$(this).attr("data-still"));
+                $(this).attr("data-state","still");
+            }
+        });
 
 
-})
+        $("#add-vg").on("click", function(event){
+            event.preventDefault();
+            let newVg = $("input").eq(0).val();
+
+            if(newVg.length > 2) {
+                videogames.push(newVg);
+            }
+
+            vgButtons(videogames, "vg-button", "#vg-buttons")
+
+        });
+
+        vgButtons(videogames, "vg-button", "#vg-buttons")
+
+
+});
